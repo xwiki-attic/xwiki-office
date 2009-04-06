f982040c3eb8390b2b75796a47dbc6434e46d7c5
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2009-04-06 13:35:04 +0000

    XOFFICE-66 Write an XWiki XmlRpc proxy for the .net framework
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@18254 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xword/XWikiLib/Clients/XmlRpc/Model/Page.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace XWiki.XmlRpc
+{
+    /// <summary>
+    /// Contains the attributes of a page.
+    /// </summary>
+    public struct Page
+    {
+        /// <summary>
+        /// The id of the parent page.
+        /// </summary>
+        public String parentId;
+        
+        /// <summary>
+        /// The title of the page.
+        /// </summary>
+        public String title;
+        
+        /// <summary>
+        /// The 'view' url of the page.
+        /// </summary>
+        public String url;
+
+        /// <summary>
+        /// Array containing all available translations for the current page.
+        /// The array does not contain the value of the default language of the page.
+        /// </summary>
+        public String[] translations;
+
+        /// <summary>
+        /// The name of the space.
+        /// </summary>
+        public String space;
+
+        /// <summary>
+        /// The id of the page.
+        /// </summary>
+        public String id;
+
+        /// <summary>
+        /// The (non-rendered)content of the page.
+        /// </summary>
+        public String content;
+    }
+}
