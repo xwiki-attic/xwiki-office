06e7687f4bcd3e19e222d60d5067ab060b849761
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2009-04-17 10:19:40 +0000

    XOFFICE-83 Incorrect tab order on add page form Patch submited by Teofil Achirei, reviewed by Florin Ciubotaru and applied with small changes.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@18774 f329d543-caf0-0310-9063-dda96c69346f

--- xword/XWord/AddPageForm.Designer.cs
+++ xword/XWord/AddPageForm.Designer.cs
@@ -61,7 +61,7 @@
             this.txtSpaceName.Location = new System.Drawing.Point(117, 55);
             this.txtSpaceName.Name = "txtSpaceName";
             this.txtSpaceName.Size = new System.Drawing.Size(284, 21);
-            this.txtSpaceName.TabIndex = 1;
+            this.txtSpaceName.TabIndex = 2;
             // 
             // groupBoxSpace
             // 
@@ -83,7 +83,7 @@
             this.comboBoxSpaceName.FormattingEnabled = true;
             this.comboBoxSpaceName.Location = new System.Drawing.Point(117, 77);
             this.comboBoxSpaceName.Name = "comboBoxSpaceName";
-            this.comboBoxSpaceName.Size = new System.Drawing.Size(284, 23);
+            this.comboBoxSpaceName.Size = new System.Drawing.Size(284, 25);
             this.comboBoxSpaceName.TabIndex = 2;
             this.comboBoxSpaceName.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -92,7 +92,7 @@
             this.radioButtonNewSpace.AutoSize = true;
             this.radioButtonNewSpace.Location = new System.Drawing.Point(125, 22);
             this.radioButtonNewSpace.Name = "radioButtonNewSpace";
-            this.radioButtonNewSpace.Size = new System.Drawing.Size(88, 19);
+            this.radioButtonNewSpace.Size = new System.Drawing.Size(97, 21);
             this.radioButtonNewSpace.TabIndex = 1;
             this.radioButtonNewSpace.TabStop = true;
             this.radioButtonNewSpace.Text = "New Space";
@@ -103,7 +103,7 @@
             this.radioButtonExistingSpace.AutoSize = true;
             this.radioButtonExistingSpace.Location = new System.Drawing.Point(7, 22);
             this.radioButtonExistingSpace.Name = "radioButtonExistingSpace";
-            this.radioButtonExistingSpace.Size = new System.Drawing.Size(106, 19);
+            this.radioButtonExistingSpace.Size = new System.Drawing.Size(118, 19);
             this.radioButtonExistingSpace.TabIndex = 0;
             this.radioButtonExistingSpace.TabStop = true;
             this.radioButtonExistingSpace.Text = "Existing Space";
@@ -128,7 +128,7 @@
             this.txtPageTitle.Location = new System.Drawing.Point(117, 63);
             this.txtPageTitle.Name = "txtPageTitle";
             this.txtPageTitle.Size = new System.Drawing.Size(284, 21);
-            this.txtPageTitle.TabIndex = 3;
+            this.txtPageTitle.TabIndex = 4;
             // 
             // label2
             // 
@@ -136,7 +136,7 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(7, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.Size = new System.Drawing.Size(72, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Page title:";
             // 
@@ -145,7 +145,7 @@
             this.txtPageName.Location = new System.Drawing.Point(117, 33);
             this.txtPageName.Name = "txtPageName";
             this.txtPageName.Size = new System.Drawing.Size(284, 21);
-            this.txtPageName.TabIndex = 1;
+            this.txtPageName.TabIndex = 3;
             // 
             // label1
             // 
@@ -153,7 +153,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.Size = new System.Drawing.Size(87, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Page name:";
             // 
@@ -163,7 +163,7 @@
             this.btnAddPage.Location = new System.Drawing.Point(276, 254);
             this.btnAddPage.Name = "btnAddPage";
             this.btnAddPage.Size = new System.Drawing.Size(77, 28);
-            this.btnAddPage.TabIndex = 4;
+            this.btnAddPage.TabIndex = 5;
             this.btnAddPage.Text = "Add page";
             this.btnAddPage.UseVisualStyleBackColor = true;
             this.btnAddPage.Click += new System.EventHandler(this.btnAddPage_Click);
@@ -174,7 +174,7 @@
             this.btnCancel.Location = new System.Drawing.Point(359, 254);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(77, 28);
-            this.btnCancel.TabIndex = 5;
+            this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
