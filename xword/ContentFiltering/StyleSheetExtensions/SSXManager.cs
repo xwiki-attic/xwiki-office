2d51d2ddd437565ac36d3b9de1039c0565403c1e
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2009-07-31 14:39:18 +0000

    XOFFICE-128 Methods for sending and getting stylesheet extensions for a page Patch submited by Teofil Achirei, applied by Florin Ciubotaru
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@22284 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xword/ContentFiltering/StyleSheetExtensions/SSXManager.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XWiki.XmlRpc;
+using System.Xml;
+using CookComputing.XmlRpc;
+using XWiki.Office.Word;
+using System.Collections.Specialized;
+using XWiki.Clients;
+
+namespace ContentFiltering.StyleSheetExtensions
+{
+    /// <summary>
+    /// StyleSheet Extensions management class. 
+    /// A SSXManager builded from cleaned HTML of a local editing page can be used to 
+    /// add StyleSheetExtensions objects coresponding to the CSS of that page.
+    /// A SSXManager builded from a remote wiki page can be used to get the CSS coresponding
+    /// to the StyleSheetExtensions objects of that page.
+    /// </summary>
+    public class SSXManager
+    {
+        const string SSX_CLASS_NAME = "XWiki.StyleSheetExtension";
+        const string XOFFICE_SSX = "XOfficeStyle";
+
+        /// <summary>
+        /// Creates a <code>SSXManager</code> from the cleaned HTML of a local editing page.
+        /// </summary>
+        /// <param name="pageConverter">An instance of the <code>ConversionManager</code> for this page.</param>
+        /// <param name="cleanHTML">Cleaned HTML of the local page.</param>
+        /// <returns>An instance of a <code>SSXManager</code>.</returns>
+        public static SSXManager BuildFromLocalHTML(ConversionManager pageConverter, string cleanHTML)
+        {
+            SSXManager ssxManager = new SSXManager();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(cleanHTML);
+            XmlNodeList styleNodes = xmlDoc.GetElementsByTagName("style");
+            string pageCSSContent = "";
+
+            foreach (XmlNode styleNode in styleNodes)
+            {
+                pageCSSContent += styleNode.InnerText;
+            }
+
+            XmlRpcStruct dictionary = new XmlRpcStruct();
+            dictionary.Add("code", pageCSSContent);
+            dictionary.Add("name", "XOfficeStyle");
+            dictionary.Add("use", "onDemand");
+            dictionary.Add("parse", "0");
+
+            XWikiObject ssxObject = new XWikiObject();
+            ssxObject.className = SSX_CLASS_NAME;
+            ssxObject.pageId = pageConverter.States.PageFullName;
+            ssxObject.objectDictionary = dictionary;
+
+            ssxManager.pageFullName = pageConverter.States.PageFullName;
+            ssxManager.pageCSSContent = pageCSSContent;
+            ssxManager.pageStyleSheetExtensions = new List<XWikiObject>();
+            ssxManager.pageStyleSheetExtensions.Add(ssxObject);
+            ssxManager.pageConverter = pageConverter;
+            return ssxManager;
+        }
+
+        /// <summary>
+        /// Builds a <code>SSXManager</code> for a remote wiki page.
+        /// </summary>
+        /// <param name="pageConverter">An instance of the <code>ConversionManager</code> for this page.</param>
+        /// <returns>An instance of a <code>SSXManager</code>.</returns>
+        public static SSXManager BuildFromServerPage(ConversionManager pageConverter)
+        {
+            SSXManager ssxManager = new SSXManager();
+            ssxManager.pageFullName = pageConverter.States.PageFullName;
+            ssxManager.pageConverter = pageConverter;
+
+            List<XWikiObject> ssxObjects = ssxManager.RetrieveStyleSheetExtensions();
+
+            ssxManager.pageStyleSheetExtensions = ssxObjects;
+            StringBuilder sb = new StringBuilder();
+            foreach (XWikiObject ssxObject in ssxObjects)
+            {
+                sb.Append(ssxObject.objectDictionary["code"]);
+                sb.Append(Environment.NewLine);
+            }
+            ssxManager.pageCSSContent = sb.ToString();
+
+            return ssxManager;
+        }
+
+
+        private ConversionManager pageConverter;
+        private string pageFullName;
+        private List<XWikiObject> pageStyleSheetExtensions;
+        private string pageCSSContent;
+
+
+        /// <summary>
+        /// An instance of SSXManager can be obtained only with its BuildFrom... methods.
+        /// </summary>
+        protected SSXManager()
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the full name of the page.
+        /// </summary>
+        public string PageFullName
+        {
+            get { return pageFullName; }
+        }
+
+        /// <summary>
+        /// Gets the CSS content for the page.
+        /// </summary>
+        public string PageCSSContent
+        {
+            get { return pageCSSContent; }
+        }
+
+        /// <summary>
+        /// Gets a copy of the StyleSheetExtension XWikiObject list.
+        /// </summary>
+        public List<XWikiObject> PageStyleSheetExtensions
+        {
+            get { return pageStyleSheetExtensions.ToList(); }
+        }
+
+
+        /// <summary>
+        /// Adds to server SSX objects for the current page.
+        /// </summary>
+        public void AddStyleSheetExtensions()
+        {
+            IXWikiClient client = pageConverter.XWikiClient;
+            int i = 0;
+            foreach (XWikiObject ssxObject in pageStyleSheetExtensions)
+            {
+                NameValueCollection fieldsValues = new NameValueCollection();
+
+                fieldsValues.Add("code", ssxObject.objectDictionary["code"].ToString());
+                fieldsValues.Add("name", ssxObject.objectDictionary["name"].ToString());
+                fieldsValues.Add("use", ssxObject.objectDictionary["use"].ToString());
+                fieldsValues.Add("parse", ssxObject.objectDictionary["parse"].ToString());
+
+                //remove existing XOffice style sheet extensions for current page
+                List<XWikiObject> existingSSXObjects = RetrieveStyleSheetExtensions();
+                foreach (XWikiObject existingSSX in existingSSXObjects)
+                {
+                    if (existingSSX.objectDictionary["name"] + "" == XOFFICE_SSX || existingSSX.prettyName == XOFFICE_SSX)
+                    {
+                        client.RemoveObject(pageFullName, SSX_CLASS_NAME, existingSSX.id);
+                    }
+                }
+
+                client.AddObject(pageFullName, ssxObject.className, fieldsValues);
+            }
+
+        }
+
+        /// <summary>
+        /// Retrieve SSX object of the current page from the server.
+        /// </summary>
+        /// <returns>A list of <code>XWikiObject</code> containing SSX objects for the current page.</returns>
+        protected List<XWikiObject> RetrieveStyleSheetExtensions()
+        {
+            XWikiObjectSummary[] objects = pageConverter.XWikiClient.GetObjects(pageFullName);
+            List<XWikiObject> ssxObjects = new List<XWikiObject>();
+            foreach (XWikiObjectSummary objSum in objects)
+            {
+                if (objSum.className == SSX_CLASS_NAME)
+                {
+                    XWikiObject obj = pageConverter.XWikiClient.GetObject(pageFullName, SSX_CLASS_NAME, objSum.id);
+                    ssxObjects.Add(obj);
+                }
+            }
+
+            return ssxObjects;
+        }
+    }
+}
