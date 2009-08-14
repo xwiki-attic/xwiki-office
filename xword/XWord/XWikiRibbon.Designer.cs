79c897e2cf37c1eb087740dbcf7e592acfdf7f03
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2009-08-14 12:26:01 +0000

    - [cleanup] removed gallery items from ribbon.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/branches/xwiki-office-1.0@22744 f329d543-caf0-0310-9063-dda96c69346f

--- xword/XWord/XWikiRibbon.Designer.cs
+++ xword/XWord/XWikiRibbon.Designer.cs
@@ -33,18 +33,6 @@
             Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem2 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
             Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem3 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
             Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem4 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem5 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem6 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem7 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem8 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem9 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem10 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem11 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem12 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem13 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem14 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem15 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
-            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItem16 = new Microsoft.Office.Tools.Ribbon.RibbonDropDownItem();
             this.tab1 = new Microsoft.Office.Tools.Ribbon.RibbonTab();
             this.XWikiTab = new Microsoft.Office.Tools.Ribbon.RibbonTab();
             this.XEGroup = new Microsoft.Office.Tools.Ribbon.RibbonGroup();
@@ -70,40 +58,12 @@
             this.dropDownSaveFormat = new Microsoft.Office.Tools.Ribbon.RibbonDropDown();
             this.separator1 = new Microsoft.Office.Tools.Ribbon.RibbonSeparator();
             this.btnAboutXWord = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.MyActivityGroup = new Microsoft.Office.Tools.Ribbon.RibbonGroup();
-            this.galMyRecentlyViewed = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button2 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button3 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button4 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.galMyRecentlyModified = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button5 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button6 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button7 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.galMyRecentlyCreated = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button8 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button9 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button10 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.TeamActivityGroup = new Microsoft.Office.Tools.Ribbon.RibbonGroup();
-            this.galTeamRecentlyView = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button11 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button12 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button13 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.galTeamRecentlyModified = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button14 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button15 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button16 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.galTeamRecentlyCreated = new Microsoft.Office.Tools.Ribbon.RibbonGallery();
-            this.button17 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button18 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
-            this.button19 = new Microsoft.Office.Tools.Ribbon.RibbonButton();
             this.tab1.SuspendLayout();
             this.XWikiTab.SuspendLayout();
             this.XEGroup.SuspendLayout();
             this.AttachmentsGroup.SuspendLayout();
             this.selectionOptionsGroup.SuspendLayout();
             this.OptionsGroup.SuspendLayout();
-            this.MyActivityGroup.SuspendLayout();
-            this.TeamActivityGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
@@ -119,8 +79,6 @@
             this.XWikiTab.Groups.Add(this.AttachmentsGroup);
             this.XWikiTab.Groups.Add(this.selectionOptionsGroup);
             this.XWikiTab.Groups.Add(this.OptionsGroup);
-            this.XWikiTab.Groups.Add(this.MyActivityGroup);
-            this.XWikiTab.Groups.Add(this.TeamActivityGroup);
             this.XWikiTab.Label = "XWord";
             this.XWikiTab.Name = "XWikiTab";
             // 
@@ -377,210 +335,6 @@
             this.btnAboutXWord.SuperTip = "Displays a summay about this Word Extension.";
             this.btnAboutXWord.Click += new System.EventHandler<Microsoft.Office.Tools.Ribbon.RibbonControlEventArgs>(this.btnAboutXWord_Click);
             // 
-            // MyActivityGroup
-            // 
-            this.MyActivityGroup.Items.Add(this.galMyRecentlyViewed);
-            this.MyActivityGroup.Items.Add(this.galMyRecentlyModified);
-            this.MyActivityGroup.Items.Add(this.galMyRecentlyCreated);
-            this.MyActivityGroup.Label = "My Activity";
-            this.MyActivityGroup.Name = "MyActivityGroup";
-            this.MyActivityGroup.Visible = false;
-            // 
-            // galMyRecentlyViewed
-            // 
-            this.galMyRecentlyViewed.Buttons.Add(this.button2);
-            this.galMyRecentlyViewed.Buttons.Add(this.button3);
-            this.galMyRecentlyViewed.Buttons.Add(this.button4);
-            this.galMyRecentlyViewed.Enabled = false;
-            ribbonDropDownItem5.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem5.Label = "Item0";
-            ribbonDropDownItem6.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem6.Label = "Item1";
-            this.galMyRecentlyViewed.Items.Add(ribbonDropDownItem5);
-            this.galMyRecentlyViewed.Items.Add(ribbonDropDownItem6);
-            this.galMyRecentlyViewed.Label = "Recenlty Viewed";
-            this.galMyRecentlyViewed.Name = "galMyRecentlyViewed";
-            this.galMyRecentlyViewed.ShowImage = true;
-            // 
-            // button2
-            // 
-            this.button2.Label = "button2";
-            this.button2.Name = "button2";
-            // 
-            // button3
-            // 
-            this.button3.Label = "button3";
-            this.button3.Name = "button3";
-            // 
-            // button4
-            // 
-            this.button4.Label = "button4";
-            this.button4.Name = "button4";
-            // 
-            // galMyRecentlyModified
-            // 
-            this.galMyRecentlyModified.Buttons.Add(this.button5);
-            this.galMyRecentlyModified.Buttons.Add(this.button6);
-            this.galMyRecentlyModified.Buttons.Add(this.button7);
-            this.galMyRecentlyModified.Enabled = false;
-            ribbonDropDownItem7.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem7.Label = "Item0";
-            ribbonDropDownItem8.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem8.Label = "Item1";
-            this.galMyRecentlyModified.Items.Add(ribbonDropDownItem7);
-            this.galMyRecentlyModified.Items.Add(ribbonDropDownItem8);
-            this.galMyRecentlyModified.Label = "Recently Modified";
-            this.galMyRecentlyModified.Name = "galMyRecentlyModified";
-            this.galMyRecentlyModified.ShowImage = true;
-            // 
-            // button5
-            // 
-            this.button5.Label = "button2";
-            this.button5.Name = "button5";
-            // 
-            // button6
-            // 
-            this.button6.Label = "button3";
-            this.button6.Name = "button6";
-            // 
-            // button7
-            // 
-            this.button7.Label = "button4";
-            this.button7.Name = "button7";
-            // 
-            // galMyRecentlyCreated
-            // 
-            this.galMyRecentlyCreated.Buttons.Add(this.button8);
-            this.galMyRecentlyCreated.Buttons.Add(this.button9);
-            this.galMyRecentlyCreated.Buttons.Add(this.button10);
-            this.galMyRecentlyCreated.Enabled = false;
-            ribbonDropDownItem9.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem9.Label = "Item0";
-            ribbonDropDownItem10.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem10.Label = "Item1";
-            this.galMyRecentlyCreated.Items.Add(ribbonDropDownItem9);
-            this.galMyRecentlyCreated.Items.Add(ribbonDropDownItem10);
-            this.galMyRecentlyCreated.Label = "Recently Created";
-            this.galMyRecentlyCreated.Name = "galMyRecentlyCreated";
-            this.galMyRecentlyCreated.ShowImage = true;
-            // 
-            // button8
-            // 
-            this.button8.Label = "button2";
-            this.button8.Name = "button8";
-            // 
-            // button9
-            // 
-            this.button9.Label = "button3";
-            this.button9.Name = "button9";
-            // 
-            // button10
-            // 
-            this.button10.Label = "button4";
-            this.button10.Name = "button10";
-            // 
-            // TeamActivityGroup
-            // 
-            this.TeamActivityGroup.Items.Add(this.galTeamRecentlyView);
-            this.TeamActivityGroup.Items.Add(this.galTeamRecentlyModified);
-            this.TeamActivityGroup.Items.Add(this.galTeamRecentlyCreated);
-            this.TeamActivityGroup.Label = "Team Activity";
-            this.TeamActivityGroup.Name = "TeamActivityGroup";
-            this.TeamActivityGroup.Visible = false;
-            // 
-            // galTeamRecentlyView
-            // 
-            this.galTeamRecentlyView.Buttons.Add(this.button11);
-            this.galTeamRecentlyView.Buttons.Add(this.button12);
-            this.galTeamRecentlyView.Buttons.Add(this.button13);
-            this.galTeamRecentlyView.Enabled = false;
-            ribbonDropDownItem11.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem11.Label = "Item0";
-            ribbonDropDownItem12.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem12.Label = "Item1";
-            this.galTeamRecentlyView.Items.Add(ribbonDropDownItem11);
-            this.galTeamRecentlyView.Items.Add(ribbonDropDownItem12);
-            this.galTeamRecentlyView.Label = "Recently Viewed";
-            this.galTeamRecentlyView.Name = "galTeamRecentlyView";
-            this.galTeamRecentlyView.ShowImage = true;
-            // 
-            // button11
-            // 
-            this.button11.Label = "button2";
-            this.button11.Name = "button11";
-            // 
-            // button12
-            // 
-            this.button12.Label = "button3";
-            this.button12.Name = "button12";
-            // 
-            // button13
-            // 
-            this.button13.Label = "button4";
-            this.button13.Name = "button13";
-            // 
-            // galTeamRecentlyModified
-            // 
-            this.galTeamRecentlyModified.Buttons.Add(this.button14);
-            this.galTeamRecentlyModified.Buttons.Add(this.button15);
-            this.galTeamRecentlyModified.Buttons.Add(this.button16);
-            this.galTeamRecentlyModified.Enabled = false;
-            ribbonDropDownItem13.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem13.Label = "Item0";
-            ribbonDropDownItem14.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem14.Label = "Item1";
-            this.galTeamRecentlyModified.Items.Add(ribbonDropDownItem13);
-            this.galTeamRecentlyModified.Items.Add(ribbonDropDownItem14);
-            this.galTeamRecentlyModified.Label = "Recently Modified";
-            this.galTeamRecentlyModified.Name = "galTeamRecentlyModified";
-            this.galTeamRecentlyModified.ShowImage = true;
-            // 
-            // button14
-            // 
-            this.button14.Label = "button2";
-            this.button14.Name = "button14";
-            // 
-            // button15
-            // 
-            this.button15.Label = "button3";
-            this.button15.Name = "button15";
-            // 
-            // button16
-            // 
-            this.button16.Label = "button4";
-            this.button16.Name = "button16";
-            // 
-            // galTeamRecentlyCreated
-            // 
-            this.galTeamRecentlyCreated.Buttons.Add(this.button17);
-            this.galTeamRecentlyCreated.Buttons.Add(this.button18);
-            this.galTeamRecentlyCreated.Buttons.Add(this.button19);
-            this.galTeamRecentlyCreated.Enabled = false;
-            ribbonDropDownItem15.Image = global::XWord.Properties.Resources.browser;
-            ribbonDropDownItem15.Label = "Item0";
-            ribbonDropDownItem16.Image = global::XWord.Properties.Resources.Crystal_Clear_action_edit_add;
-            ribbonDropDownItem16.Label = "Item1";
-            this.galTeamRecentlyCreated.Items.Add(ribbonDropDownItem15);
-            this.galTeamRecentlyCreated.Items.Add(ribbonDropDownItem16);
-            this.galTeamRecentlyCreated.Label = "Recently Created";
-            this.galTeamRecentlyCreated.Name = "galTeamRecentlyCreated";
-            this.galTeamRecentlyCreated.ShowImage = true;
-            // 
-            // button17
-            // 
-            this.button17.Label = "button2";
-            this.button17.Name = "button17";
-            // 
-            // button18
-            // 
-            this.button18.Label = "button3";
-            this.button18.Name = "button18";
-            // 
-            // button19
-            // 
-            this.button19.Label = "button4";
-            this.button19.Name = "button19";
-            // 
             // XWikiRibbon
             // 
             this.Name = "XWikiRibbon";
@@ -600,10 +354,6 @@
             this.selectionOptionsGroup.PerformLayout();
             this.OptionsGroup.ResumeLayout(false);
             this.OptionsGroup.PerformLayout();
-            this.MyActivityGroup.ResumeLayout(false);
-            this.MyActivityGroup.PerformLayout();
-            this.TeamActivityGroup.ResumeLayout(false);
-            this.TeamActivityGroup.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -670,26 +420,6 @@
         /// Edit the selected page with Word.
         /// </summary>
         public Microsoft.Office.Tools.Ribbon.RibbonButton btnEditPage;
-        internal Microsoft.Office.Tools.Ribbon.RibbonGroup MyActivityGroup;
-        internal Microsoft.Office.Tools.Ribbon.RibbonGroup TeamActivityGroup;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button2;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button3;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button4;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button5;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button6;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button7;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button8;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button9;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button10;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button11;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button12;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button13;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button14;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button15;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button16;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button17;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button18;
-        private Microsoft.Office.Tools.Ribbon.RibbonButton button19;
         /// <summary>
         /// Contains the syntax options to save a pege on the wiki.
         /// </summary>
@@ -700,12 +430,6 @@
         public Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownSaveFormat;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btnAboutXWord;
         internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galMyRecentlyViewed;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galMyRecentlyModified;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galMyRecentlyCreated;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galTeamRecentlyView;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galTeamRecentlyModified;
-        private Microsoft.Office.Tools.Ribbon.RibbonGallery galTeamRecentlyCreated;
     }
 
     partial class ThisRibbonCollection : Microsoft.Office.Tools.Ribbon.RibbonReadOnlyCollection
