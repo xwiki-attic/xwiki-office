dbacbc3600c49d25cb195727911c3ccf7c8f1bbd
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2009-08-19 16:12:09 +0000

    XOFFICE-138 Create UICommons project to contain shared addins UI - patch submited by Teofil Achirei, reviewed by Florin Ciubotaru
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@22791 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xword/UICommons/UIActionsManagement/IActionsManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace UICommons.UIActionsManagement
+{
+    /// <summary>
+    /// Generic actions manager interface implemented by all specific actions managers.
+    /// Defines a method of enqueuing event handlers for a generic control.
+    /// </summary>
+    /// <typeparam name="T">A reference to a generic control.</typeparam>
+    public interface IActionsManager<T> where T:Control
+    {
+        /// <summary>
+        /// Enqueues all (known) event handlers defined for a control.
+        /// </summary>
+        /// <param name="control">A reference to a generic control.</param>
+        void EnqueueAllHandlers(ref T control);
+    }
+}
