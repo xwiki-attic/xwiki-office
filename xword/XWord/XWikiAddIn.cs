0fe9199f104c1ec3242baaa3bc941bb801765bce
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2010-01-25 13:29:32 +0000

    - added missing LGPL license to files
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@26361 f329d543-caf0-0310-9063-dda96c69346f

--- xword/XWord/XWikiAddIn.cs
+++ xword/XWord/XWikiAddIn.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿#region LGPL license
+
+/*
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
  *
@@ -17,6 +19,8 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
+#endregion //license
 
 using System;
 using System.Collections.Generic;
