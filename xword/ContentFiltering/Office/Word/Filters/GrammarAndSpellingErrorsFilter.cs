9e84c274184811a2a16783f83c2c274c7c320de3
Florin Ciubotaru <florin.ciubotaru@xwiki.com>
2010-05-07 09:42:20 +0000

    XOFFICE-228 Null reference on Grammar filter
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xoffice/trunk@28774 f329d543-caf0-0310-9063-dda96c69346f

--- xword/ContentFiltering/Office/Word/Filters/GrammarAndSpellingErrorsFilter.cs
+++ xword/ContentFiltering/Office/Word/Filters/GrammarAndSpellingErrorsFilter.cs
@@ -26,9 +26,11 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using XWiki.Office.Word;
+using XWiki;
+
 namespace ContentFiltering.Office.Word.Filters
 {
-    public class GrammarAndSpellingErrorsFilter:IDOMFilter
+    public class GrammarAndSpellingErrorsFilter : IDOMFilter
     {
         private ConversionManager manager;
 
@@ -38,7 +40,7 @@ namespace ContentFiltering.Office.Word.Filters
         }
 
         #region IDOMFilter Members
-        
+
         /// <summary>
         /// Removes 'class' attribute from text marked as  containing grammar or spelling errors.
         /// (when values are 'gramE' or 'spellE'). Removes 'lang' attribute. Adds a space character
@@ -52,39 +54,49 @@ namespace ContentFiltering.Office.Word.Filters
             XmlNode tempNode = null;
             foreach (XmlNode node in nodes)
             {
-                insertASpace = false;
-                XmlAttribute classAttribute = node.Attributes["class"];
-                if (classAttribute != null)
+                try
                 {
-                    if (classAttribute.Value.ToLower().Trim().IndexOf("grame") >= 0
-                        ||
-                        classAttribute.Value.ToLower().Trim().IndexOf("spelle") >= 0)
+                    insertASpace = false;
+                    XmlAttribute classAttribute = node.Attributes["class"];
+                    if (classAttribute != null)
                     {
-                        node.Attributes.Remove(classAttribute);
+                        if (classAttribute.Value.ToLower().Trim().IndexOf("grame") >= 0
+                            ||
+                            classAttribute.Value.ToLower().Trim().IndexOf("spelle") >= 0)
+                        {
+                            node.Attributes.Remove(classAttribute);
+                            insertASpace = true;
+                        }
+                    }
+                    XmlAttribute langAttribute = node.Attributes["lang"];
+                    if (langAttribute != null)
+                    {
+                        node.Attributes.Remove(langAttribute);
                         insertASpace = true;
                     }
-                }
-                XmlAttribute langAttribute = node.Attributes["lang"];
-                if (langAttribute != null)
-                {
-                    node.Attributes.Remove(langAttribute);
-                    insertASpace = true;
-                }
 
-                if (insertASpace)
+                    if (insertASpace)
+                    {
+                        if (node.NodeType == XmlNodeType.Element)
+                        {
+                            tempNode = node.ChildNodes[0];
+                        }
+                        else
+                        {
+                            tempNode = node;
+                        }
+                        if (tempNode != null)
+                        {
+                            if (tempNode.Value != null)
+                            {
+                                tempNode.Value += " ";
+                            }
+                        }
+                    }
+                }
+                catch (NullReferenceException nre)
                 {
-                    if (node.NodeType == XmlNodeType.Element)
-                    {
-                        tempNode = node.ChildNodes[0];
-                    }
-                    else
-                    {
-                        tempNode = node;
-                    }
-                    if (tempNode.Value != null)
-                    {
-                        tempNode.Value += " ";
-                    }
+                    Log.Exception(nre);
                 }
             }
         }
